416aca54aef6afe4e6a58932a9dae9f06860f59c
FredNekrasov <frednekrasov49@gmail.com>
2024-04-30 15:11:07 +0300

    подключение зависимостей во все модули

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,19 @@ android {
 }
 
 dependencies {
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
+
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -33,10 +33,17 @@ android {
 }
 
 dependencies {
+    implementation(libs.koin.core)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
